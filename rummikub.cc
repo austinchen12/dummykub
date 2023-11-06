587ddbd69a20e09ff04f7db031ca005b89b8bd82
Austin Chen <austin12chen@gmail.com>
2023-11-06 11:27:14 -0500

    revised

--- rummikub.cc
+++ rummikub.cc
@@ -7,6 +7,11 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include <array>
+#include <tuple>
 using namespace std;
 
 template <int const base, unsigned int const exponent>
@@ -25,13 +30,23 @@ struct Power<base, 0>
 		value = 1
 	};
 };
+struct Tile
+{
+	int number;
+	std::string color;
+	Tile(int num, const std::string &col) : number(num), color(col) {}
+};
 
-const int N = 100;
+const int N = 1 + 13;
 const int K = 4;
 const int M = 2;
 const int KIJK = 10;
+const int MIN_SET_SIZE = 3;
+
+const array<std::string, K> COLORS = {"BLUE", "BLACK", "RED", "YELLOW"};
 
 int minn, maxn;
+int original_board[K][N + 1];
 int hand[K][N + 1];
 int dp[N + 1][Power<K, K * M>::value];
 int next_array[N + 1][Power<K, K * M>::value];
@@ -41,56 +56,344 @@ int mp(const int *inlengths)
 	int newlengths[K * M];
 	for (int i = 0; i < K; i++)
 	{
-		if (inlengths[i] >= inlengths[K + i])
-		{
-			newlengths[i] = inlengths[i];
-			newlengths[i + K] = inlengths[i + K];
-		}
-		else
-		{
-			newlengths[i] = inlengths[i + K];
-			newlengths[i + K] = inlengths[i];
-		}
+		newlengths[i] = inlengths[i];
+		newlengths[i + K] = inlengths[i + K];
 	}
+
+	int sum = 0;
+	for (int i = 0; i < K * M; i++)
+		sum += newlengths[i] * pow(K, i);
+
+	return sum;
 	return newlengths[0] + K * newlengths[1] + K * K * newlengths[2] + K * K * K * newlengths[3] + K * K * K * K * newlengths[4] + K * K * K * K * K * newlengths[5] + K * K * K * K * K * K * newlengths[6] + K * K * K * K * K * K * K * newlengths[7]; // now for K=4, should be made for any K
 } // mp
 
 int go(int, int *); // header for go-function
 
-/*
-int mp(const int * inlengths) { // map runlengths array to dp array index - SLOW
-	int sum = 0;
-	for(int i=0; i<K*M; i++)
-		sum += pow(K, i) * inlengths[i];
-	return sum;
-} // mp, 15%-3000% slower
-*/
-
-/*
-void rmp(int in, int * result) { // reverse mapping from int to runlengt-array
+void rmp(int in, int *result)
+{ // reverse mapping from int to runlengt-array
 	int i = 0;
-	while(in > 0) {
+	while (in > 0)
+	{
 		result[i] = in % K;
 		in = in / K;
 		i++;
 	} // while
 } // rmp
 
-void show(int val, int * inlengths) {  // prints the solution of the problem
+map<pair<tuple<int, int>, tuple<int, int>>, int> rest = {
+	{make_pair(make_tuple(0, 1), make_tuple(0, 2)), 2},
+	{make_pair(make_tuple(0, 1), make_tuple(1, 2)), 3},
+	{make_pair(make_tuple(0, 2), make_tuple(0, 3)), 2},
+	{make_pair(make_tuple(0, 2), make_tuple(1, 3)), 3},
+	{make_pair(make_tuple(0, 3), make_tuple(0, 3)), 2},
+	{make_pair(make_tuple(0, 3), make_tuple(1, 3)), 3},
+	{make_pair(make_tuple(1, 0), make_tuple(2, 0)), 1},
+	{make_pair(make_tuple(1, 0), make_tuple(2, 1)), 3},
+	{make_pair(make_tuple(2, 0), make_tuple(3, 0)), 1},
+	{make_pair(make_tuple(2, 0), make_tuple(3, 1)), 3},
+	{make_pair(make_tuple(3, 0), make_tuple(3, 0)), 1},
+	{make_pair(make_tuple(3, 0), make_tuple(3, 1)), 3},
+	{make_pair(make_tuple(1, 1), make_tuple(2, 0)), 1},
+	{make_pair(make_tuple(1, 1), make_tuple(0, 2)), 2},
+	{make_pair(make_tuple(1, 1), make_tuple(2, 2)), 3},
+	{make_pair(make_tuple(1, 1), make_tuple(2, 1)), 3},
+	{make_pair(make_tuple(1, 1), make_tuple(1, 2)), 3},
+	{make_pair(make_tuple(1, 2), make_tuple(2, 0)), 1},
+	{make_pair(make_tuple(1, 2), make_tuple(0, 3)), 2},
+	{make_pair(make_tuple(1, 2), make_tuple(2, 3)), 3},
+	{make_pair(make_tuple(1, 2), make_tuple(2, 1)), 3},
+	{make_pair(make_tuple(1, 2), make_tuple(1, 3)), 3},
+	{make_pair(make_tuple(1, 3), make_tuple(2, 0)), 1},
+	{make_pair(make_tuple(1, 3), make_tuple(0, 3)), 2},
+	{make_pair(make_tuple(1, 3), make_tuple(2, 3)), 3},
+	{make_pair(make_tuple(1, 3), make_tuple(2, 1)), 3},
+	{make_pair(make_tuple(1, 3), make_tuple(1, 3)), 3},
+	{make_pair(make_tuple(2, 1), make_tuple(3, 0)), 1},
+	{make_pair(make_tuple(2, 1), make_tuple(0, 2)), 2},
+	{make_pair(make_tuple(2, 1), make_tuple(3, 2)), 3},
+	{make_pair(make_tuple(2, 1), make_tuple(3, 1)), 3},
+	{make_pair(make_tuple(2, 1), make_tuple(1, 2)), 3},
+	{make_pair(make_tuple(2, 2), make_tuple(3, 0)), 1},
+	{make_pair(make_tuple(2, 2), make_tuple(0, 3)), 2},
+	{make_pair(make_tuple(2, 2), make_tuple(3, 3)), 3},
+	{make_pair(make_tuple(2, 2), make_tuple(3, 1)), 3},
+	{make_pair(make_tuple(2, 2), make_tuple(1, 3)), 3},
+	{make_pair(make_tuple(2, 3), make_tuple(3, 0)), 1},
+	{make_pair(make_tuple(2, 3), make_tuple(0, 3)), 2},
+	{make_pair(make_tuple(2, 3), make_tuple(3, 3)), 3},
+	{make_pair(make_tuple(2, 3), make_tuple(3, 1)), 3},
+	{make_pair(make_tuple(2, 3), make_tuple(1, 3)), 3},
+	{make_pair(make_tuple(3, 1), make_tuple(3, 0)), 1},
+	{make_pair(make_tuple(3, 1), make_tuple(0, 2)), 2},
+	{make_pair(make_tuple(3, 1), make_tuple(3, 2)), 3},
+	{make_pair(make_tuple(3, 1), make_tuple(3, 1)), 3},
+	{make_pair(make_tuple(3, 1), make_tuple(1, 2)), 3},
+	{make_pair(make_tuple(3, 2), make_tuple(3, 0)), 1},
+	{make_pair(make_tuple(3, 2), make_tuple(0, 3)), 2},
+	{make_pair(make_tuple(3, 2), make_tuple(3, 3)), 3},
+	{make_pair(make_tuple(3, 2), make_tuple(3, 1)), 3},
+	{make_pair(make_tuple(3, 2), make_tuple(1, 3)), 3},
+	{make_pair(make_tuple(3, 3), make_tuple(3, 0)), 1},
+	{make_pair(make_tuple(3, 3), make_tuple(0, 3)), 2},
+	{make_pair(make_tuple(3, 3), make_tuple(3, 3)), 3},
+	{make_pair(make_tuple(3, 3), make_tuple(3, 1)), 3},
+	{make_pair(make_tuple(3, 3), make_tuple(1, 3)), 3},
+};
+
+int get_tiles_placed_from_lengths(tuple<int, int> prev_lengths, tuple<int, int> curr_lengths)
+{
+	if (curr_lengths == make_tuple(0, 0))
+	{
+		return 0;
+	}
+	else if (curr_lengths == make_tuple(1, 0))
+	{
+		return 1;
+	}
+	else if (curr_lengths == make_tuple(0, 1))
+	{
+		return 2;
+	}
+	else if (curr_lengths == make_tuple(1, 1))
+	{
+		return 3;
+	}
+
+	auto it = rest.find(make_pair(prev_lengths, curr_lengths));
+	if (it != rest.end())
+	{
+		return it->second;
+	}
+
+	return -1;
+}
+
+void show(int val, int *inlengths)
+{
+	int prev_arr[K * M] = {0};
+	int curr_arr[K * M] = {0};
+	memcpy(curr_arr, inlengths, sizeof(curr_arr));
 	int current = mp(inlengths);
-	int c[K*M] = {0};
-	while(current >= 0) {
-		memset(c, 0, sizeof(c));
-		rmp(current, c);
-		////cerr<< val-1 << "\t" << current << "\t" << next_array[val][current] <<  "\t";
-		for(int i=0; i<K; i++)
-			 ////cerr<< i << ": " << c[i] << "-" << c[i+K] << "\t";
-		////cerr<< dp[val][current] << endl;
+
+	int true_lengths[K * M] = {0};
+	vector<vector<Tile>> sets;
+
+	while (current >= 0)
+	{
+		memcpy(prev_arr, curr_arr, sizeof(prev_arr));
+		memset(curr_arr, 0, sizeof(curr_arr));
 		current = next_array[val][current];
-		val++;
-	} // while
-} // show
-*/
+		rmp(current, curr_arr);
+		for (int k = 0; k < K; ++k)
+		{
+			int i1 = k, i2 = K + k;
+			int prev1_length = prev_arr[i1];
+			int curr1_length = curr_arr[i1];
+			int prev2_length = prev_arr[i2];
+			int curr2_length = curr_arr[i2];
+			int count = get_tiles_placed_from_lengths(tuple<int, int>(prev1_length, prev2_length), tuple<int, int>(curr1_length, curr2_length));
+			assert(count != -1);
+
+			if (prev1_length >= 3 && curr1_length == 0)
+			{
+				vector<Tile> run;
+				for (int i = val - true_lengths[i1]; i < val; ++i)
+				{
+					run.push_back(Tile(i, COLORS[k]));
+				}
+				sets.push_back(run);
+			}
+			if (prev2_length >= 3 && curr2_length == 0)
+			{
+				vector<Tile> run;
+				for (int i = val - true_lengths[i2]; i < val; ++i)
+				{
+					run.push_back(Tile(i, COLORS[k]));
+				}
+				sets.push_back(run);
+			}
+
+			if (curr1_length == 0)
+			{
+				true_lengths[i1] = 0;
+			}
+			if (curr2_length == 0)
+			{
+				true_lengths[i2] = 0;
+			}
+
+			if (count == 1)
+			{
+				true_lengths[i1] += 1;
+			}
+			else if (count == 2)
+			{
+				true_lengths[i2] += 1;
+			}
+			else if (count == 3)
+			{
+				true_lengths[i1] += 1;
+				true_lengths[i2] += 1;
+			}
+		}
+		val += 1;
+	}
+
+	int tile_counts[K][N + 1];
+	memcpy(tile_counts, hand, sizeof(tile_counts));
+
+	for (const vector<Tile> &run : sets)
+	{
+		for (const Tile &tile : run)
+		{
+			int color_index = distance(COLORS.begin(), find(COLORS.begin(), COLORS.end(), tile.color));
+			tile_counts[color_index][tile.number] -= 1;
+		}
+	}
+
+	vector<Tile> unused_tiles;
+	for (int i = 1; i <= N; ++i)
+	{
+		int grouparray[K] = {0};
+		for (int k = 0; k < K; ++k)
+		{
+			grouparray[k] = tile_counts[k][i];
+		}
+
+		vector<pair<int, int>> ones, twos;
+		for (int k = 0; k < K; ++k)
+		{
+			if (grouparray[k] >= 1)
+			{
+				ones.push_back(make_pair(i, k));
+			}
+			if (grouparray[k] == 2)
+			{
+				twos.push_back(make_pair(i, k));
+			}
+		}
+
+		if (ones.size() >= 3 && twos.size() >= 3)
+		{
+			vector<Tile> group1, group2;
+			for (const auto &tile : ones)
+			{
+				group1.push_back(Tile(tile.first, COLORS[tile.second]));
+			}
+			for (const auto &tile : twos)
+			{
+				group2.push_back(Tile(tile.first, COLORS[tile.second]));
+			}
+			sets.push_back(group1);
+			sets.push_back(group2);
+
+			for (const auto &tile : ones)
+			{
+				int color_index = tile.second;
+				int number = tile.first;
+				tile_counts[color_index][number] -= 1;
+			}
+			for (const auto &tile : twos)
+			{
+				int color_index = tile.second;
+				int number = tile.first;
+				tile_counts[color_index][number] -= 1;
+			}
+		}
+		else if (ones.size() == 4 && twos.size() == 2)
+		{
+			// find the first tile that is in ones but not in twos
+			vector<Tile> group1, group2;
+			bool found = false;
+			for (const auto &tile : ones)
+			{
+				if (!found && find(twos.begin(), twos.end(), tile) == twos.end())
+				{
+					found = true;
+					group2.push_back(Tile(tile.first, COLORS[tile.second]));
+				}
+				else
+				{
+					group1.push_back(Tile(tile.first, COLORS[tile.second]));
+				}
+			}
+			for (const auto &tile : twos)
+			{
+				group2.push_back(Tile(tile.first, COLORS[tile.second]));
+			}
+			sets.push_back(group1);
+			sets.push_back(group2);
+
+			for (const auto &tile : ones)
+			{
+				int color_index = tile.second;
+				int number = tile.first;
+				tile_counts[color_index][number] -= 1;
+			}
+			for (const auto &tile : twos)
+			{
+				int color_index = tile.second;
+				int number = tile.first;
+				tile_counts[color_index][number] -= 1;
+			}
+		}
+		else if (ones.size() >= 3)
+		{
+			vector<Tile> group1;
+			for (const auto &tile : ones)
+			{
+				group1.push_back(Tile(tile.first, COLORS[tile.second]));
+			}
+			sets.push_back(group1);
+
+			for (const auto &tile : ones)
+			{
+				int color_index = tile.second;
+				int number = tile.first;
+				tile_counts[color_index][number] -= 1;
+			}
+		}
+	}
+
+	for (int k = 0; k < K; ++k)
+	{
+		for (int i = 1; i <= N; ++i)
+		{
+			for (int _ = 0; _ < tile_counts[k][i]; ++_)
+			{
+				unused_tiles.push_back(Tile(i, COLORS[k]));
+			}
+		}
+	}
+
+	for (const auto &group : sets)
+	{
+		cout << '[';
+		for (const auto &tile : group)
+		{
+			cout << tile.number << tile.color;
+			if (&tile != &group.back())
+			{
+				cout << ", ";
+			}
+		}
+		cout << ']' << endl;
+	}
+
+	cout << endl;
+
+	cout << '[';
+	for (const auto &tile : unused_tiles)
+	{
+		cout << tile.number << tile.color;
+		if (&tile != &unused_tiles.back())
+		{
+			cout << ", ";
+		}
+	}
+	cout << ']';
+}
 
 void makegroups(int value, int *inlengths, int *runlengths, int *runscores)
 {
@@ -210,43 +513,58 @@ int go(int value, int *inlengths)
 
 int main(int argc, char *argv[])
 {
-	int runs, n, value;
-	char c;
-	cin >> runs;
-	while (runs--)
+	memset(original_board, 0, sizeof(original_board));
+	memset(hand, 0, sizeof(hand));
+	memset(dp, -1, sizeof(dp));
+	memset(next_array, -1, sizeof(dp));
+	maxn = 0;
+	minn = N + 1;
+
+	bool seen_dash = false;
+	for (int i = 1; i < argc; i++)
 	{
-		memset(hand, 0, sizeof(hand));
-		memset(dp, -1, sizeof(dp));
-		memset(next_array, -1, sizeof(dp));
-		maxn = 0;
-		minn = N + 1;
-		cin >> n;
-		for (int i = 0; i < n; i++)
+		std::string tile = argv[i];
+		if (tile[0] == '-')
 		{
-			cin >> value >> c;
-			maxn = max(maxn, value);
-			minn = min(minn, value);
-			switch (c)
-			{
-			case 'b':
-				hand[0][value]++;
-				break;
-			case 'g':
-				hand[1][value]++;
-				break;
-			case 'r':
-				hand[2][value]++;
-				break;
-			case 'y':
-				hand[3][value]++;
-				break;
-			default:
-				break;
-			} // switch
-		}	  // for
-		int inlengths[K * M] = {0};
-		cout << go(minn, inlengths) << endl;
-		// show(minn, inlengths);
-	} // while
+			seen_dash = true;
+			continue;
+		}
+
+		char c = tile[tile.length() - 1];
+		int value = atoi(tile.substr(0, tile.length() - 1).c_str());
+
+		maxn = max(maxn, value);
+		minn = min(minn, value);
+		switch (c)
+		{
+		case 'b':
+			hand[0][value]++;
+			if (!seen_dash)
+				original_board[0][value]++;
+			break;
+		case 'g':
+			hand[1][value]++;
+			if (!seen_dash)
+				original_board[1][value]++;
+			break;
+		case 'r':
+			hand[2][value]++;
+			if (!seen_dash)
+				original_board[2][value]++;
+			break;
+		case 'y':
+			hand[3][value]++;
+			if (!seen_dash)
+				original_board[3][value]++;
+			break;
+		default:
+			break;
+		} // switch
+	}	  // for
+
+	int inlengths[K * M] = {0};
+	cout << go(minn, inlengths) << endl
+		 << endl;
+	show(minn, inlengths);
 	return 0;
 } // main
